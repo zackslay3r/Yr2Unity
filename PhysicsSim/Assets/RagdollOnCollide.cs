ceb87c6b7e50e6f96045794cfca11c34038411ce
Unknown <zackary.direen@hotmail.com>
2018-03-05 22:10:30 +1100

    test sim

---
+++ PhysicsSim/Assets/RagdollOnCollide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnCollide : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ragdoll ragdoll = other.gameObject.GetComponentInParent<Ragdoll>();
+        if (ragdoll != null)
+        {
+            ragdoll.RagdollOn = true;
+        }
+    }
+}
